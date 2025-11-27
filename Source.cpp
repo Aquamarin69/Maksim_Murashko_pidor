780b8df508043e845490ae4d5f57a8788c1ce921
1JOFFREY1 <99999999ghw@gmail.com>
2025-11-27 19:06:59 +0300

    1JOFFREY1

--- Source.cpp
+++ Source.cpp
@@ -1,27 +1,21 @@
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "SFML window");
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
     while (window.isOpen())
     {
-        // Process events
         while (const std::optional event = window.pollEvent())
         {
-            // Close window: exit
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
-        // Clear screen
         window.clear();
-
-        
-       
-
-        // Update the window
+        window.draw(shape);
         window.display();
     }
 }
